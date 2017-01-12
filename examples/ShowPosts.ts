d992bc39406b4cc42dd806780f0835ac814004a6
Niklas Mollenhauer <nikeee@outlook.com>
2017-01-12 17:22:24 +0100

    Reformat sample

--- examples/ShowPosts.ts
+++ examples/ShowPosts.ts
@@ -7,8 +7,8 @@ type JodelVersion = "4.27.0" | "4.28.1" | "4.29.1" | "4.30.2" | "4.31.1";
 
 // See Keys.md
 const jodelKeys: JodelKeyConfigs<JodelVersion> = {
-    /* "jodelVersion": {key: "key", clientVersion: "jodelVersion", apiVersion: "0.2"} */
-    "4.31.1": { key: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", clientVersion: "4.31.1", apiVersion: "0.2" },
+	/* "jodelVersion": {key: "key", clientVersion: "jodelVersion", apiVersion: "0.2"} */
+	"4.31.1": { key: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", clientVersion: "4.31.1", apiVersion: "0.2" },
 };
 
 const keyConfig = jodelKeys["4.31.1"];
@@ -21,31 +21,34 @@ const uid = AndroidJodelConfig.createDeviceUID();
 const config = new AndroidJodelConfig(uid, keyConfig);
 
 async function main() {
-    try
-    {
-        const client = new JodelClient(config);
-        console.log("Loggin in...");
+	try {
+		const client = new JodelClient(config);
+		console.log("Loggin in...");
 
-        await client.login({
-            city: "",
-            country: "DE",
-            locAccuracy: 10,
-            locCoordinates: {
-                lat: 0,
-                lng: 0
-            }
-        });
-        console.log("Logged in!");
-        console.log("Token: " + client.accessToken);
-        // the token can be passed to JodelClient#loginWithToken to use the same token to login back again.
+		await client.login({
+			city: "",
+			country: "DE",
+			locAccuracy: 10,
+			locCoordinates: {
+				lat: 0,
+				lng: 0
+			}
+		});
+		console.log("Logged in!");
+		console.log("Token: " + client.accessToken);
+		// the token can be passed to JodelClient#loginWithToken to use the same token to login back again.
 
-        const res = await client.getKarma();
-        console.log("Karma: " + res.karma);
-    }
-    catch (ex)
-    {
-        console.error("Something failed: ", ex);
-    }
+		const res = await client.getKarma();
+		console.log("Karma: " + res.karma);
+		const posts = await client.getMostRecentPosts();
+		for (const post of posts.posts) {
+			console.log(post.message);
+			console.log("------------------------------");
+		}
+	}
+	catch (ex) {
+		console.error("Something failed: ", ex);
+	}
 }
 
 main();
